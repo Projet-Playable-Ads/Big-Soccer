27a38c8e22137f3714c1d9743743d075986617b7
capinho <papadiawara@hotmail.fr>
2023-03-21 16:48:46 +0100

    add shoot on the ball function

--- src/index.js
+++ src/index.js
@@ -1,33 +1,90 @@
 import * as PIXI from 'pixi.js';
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
 const app = new PIXI.Application();
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
 document.body.appendChild(app.view);
 
-// load the texture we need
-app.loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
+app.loader.add('ball', 'assets/ball.png')
+          .add('goal', 'assets/goal.png')
+          .add('goalAnimation', 'assets/but.jpeg')
+          .load(setup);
 
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+function setup() {
 
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+  // Create the ball 
+  const ball = new PIXI.Sprite(app.loader.resources.ball.texture);
+  ball.anchor.set(0.5);
+  ball.width = 50; 
+  ball.height = 50;
+  ball.position.set(app.screen.width / 2, app.screen.height - 100);
+  ball.interactive = true;
+  ball.buttonMode = true;
+  ball.on('pointerdown', onBallClick);
+  app.stage.addChild(ball);
 
-    // Add the bunny to the scene we are building
-    app.stage.addChild(bunny);
+  // Create the goal 
+  const goal = new PIXI.Sprite(app.loader.resources.goal.texture);
+  goal.anchor.set(0.5);
+  goal.width = 200; 
+  goal.height = 100; 
+  goal.position.set(app.screen.width / 2, 100);
+  app.stage.addChild(goal);
+  
+    
+  // Create the goal animation
+  const goalAnimation = new PIXI.AnimatedSprite([
+    app.loader.resources.goal.texture,
+    app.loader.resources.goalAnimation.texture,
+  ]);
+  goalAnimation.anchor.set(0.5);
+  goalAnimation.width = 200;
+  goalAnimation.height = 100;
+  goalAnimation.position.set(app.screen.width / 2, 100);
+  goalAnimation.animationSpeed = 0.1;
+  goalAnimation.visible = false;
+  app.stage.addChild(goalAnimation);
 
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
-});
+
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+    if (ball.isAirborne) {
+      ball.y -= 10;
+      if (ball.y < 0) {
+        ball.isAirborne = false;
+        if (ball.x > goal.x - goal.width / 2 && ball.x < goal.x + goal.width / 2) {
+          // Ball has reached the goal
+          ball.position.set(app.screen.width / 2, app.screen.height - 100);
+          ball.isAirborne = false;
+          goalAnimation.visible = true;
+          goalAnimation.play();
+          setTimeout(() => {
+            goalAnimation.visible = false;
+            
+          }, 3000); 
+        } else {
+          ball.position.set(app.screen.width / 2, app.screen.height - 100);
+        }
+      }
+    }
+  });
+
+  
+  function onBallClick() {
+    ball.isAirborne = true;
+    const goalPosition = goal.position;
+    const ballPosition = ball.position;
+    const dx = goalPosition.x - ballPosition.x;
+    const dy = goalPosition.y - ballPosition.y;
+    const angle = Math.atan2(dy, dx);
+    const speed = 20;
+    const velocity = {
+      x: speed * Math.cos(angle),
+      y: speed * Math.sin(angle)
+    };
+    ball.vx = velocity.x;
+    ball.vy = velocity.y;
+  }
+}
+
+
